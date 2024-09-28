5bffefbce86a5807b3bdb92cc50733da8a4e48e4
Yaman Khashashneh <ymankh1997@gmail.com>
2024-09-28 21:50:32 +0300

    Add project files.

---
+++ MyFirstAngularApp.Server/Program.cs
@@ -0,0 +1,46 @@
+namespace MyFirstAngularApp.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                //Use proxy to the Angular development server
+                app.UseSpa(spa =>
+                {
+                    spa.Options.SourcePath = "ClientApp"; // Path to Angular project
+
+                    // Start Angular dev server in development
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200"); // Adjust the port for Angular
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
